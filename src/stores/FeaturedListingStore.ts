63a0d2d1e08690f95e470c8ad22952d208c41e37
Matt Harrison <matt.harrison@traderinteractive.com>
2023-07-18 16:02:26 -0400

    TIDE-2 resolved type conflicts in dummy data ajax calls.

--- src/stores/FeaturedListingStore.ts
+++ src/stores/FeaturedListingStore.ts
@@ -19,38 +19,10 @@ export const useFeaturedListingStore = defineStore('featuredListingStore', {
   actions: {
     getVehicles() {
       const results: unknown = dummyResponse.results;
-      const vehicles: VehicleRaw[] = results as VehicleRaw[];
+      const vehiclesRaw: Raw[] = results as Raw[];
+      const vehicles: Vehicle[] = mapResults(vehiclesRaw);
 
       this.setVehicles(vehicles);
-
-      /*
-      const adDataRaw = document.querySelector('[data-vue-selector="ad-data"]')?.innerHTML;
-      const adData = adDataRaw ? JSON.parse(adDataRaw) : {};
-
-      const params = {
-        ad_id: adData.id || '',
-        dealer_group_id: '',
-        dealer_id: adData.dealerId || '',
-        make: adData.details.Make || '',
-        make_id: '',
-        model: adData.details.Model || '',
-      };
-
-      const query = new URLSearchParams(params);
-      const querystring = query.toString();
-
-      axios
-        .get(`/search-results-data/vdp-featured?${querystring}`)
-        .then((response) => {
-          const vehiclesRaw: Raw[] = response.data.results;
-          const vehicles: Vehicle[] = mapResults(vehiclesRaw);
-
-          this.setVehicles(vehicles);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-      */
     },
     setVehicles(vehicles: Vehicle[]) {
       this.vehicles = vehicles;
--- src/stores/VehicleDetailStore.ts
+++ src/stores/VehicleDetailStore.ts
@@ -19,38 +19,10 @@ export const useVehicleDetailStore = defineStore('vehicleDetailStore', {
   actions: {
     getVehicle() {
       const results: unknown = dummyResponse.results;
-      const vehicles: VehicleRaw[] = results as VehicleRaw[];
+      const vehiclesRaw: Raw[] = results as Raw[];
+      const vehicle: Vehicle = mapResult(vehiclesRaw[0]);
 
-      this.setVehicle(vehicles[0]);
-
-      /*
-      const adDataRaw = document.querySelector('[data-vue-selector="ad-data"]')?.innerHTML;
-      const adData = adDataRaw ? JSON.parse(adDataRaw) : {};
-
-      const params = {
-        ad_id: adData.id || '',
-        dealer_group_id: '',
-        dealer_id: adData.dealerId || '',
-        make: adData.details.Make || '',
-        make_id: '',
-        model: adData.details.Model || '',
-      };
-
-      const query = new URLSearchParams(params);
-      const querystring = query.toString();
-
-      axios
-        .get(`/search-results-data/vdp-featured?${querystring}`)
-        .then((response) => {
-          const vehicleRaw: Raw[] = response.data.results;
-          const vehicle: Vehicle = mapResult(vehicleRaw[0]);
-
-          this.setVehicle(vehicle);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-      */
+      this.setVehicle(vehicle);
     },
     setVehicle(vehicle: Vehicle) {
       this.vehicle = vehicle;
