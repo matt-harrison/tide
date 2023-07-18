bc9375b08f151ecadc7b47b8c3947470a66eb2bb
Matt Harrison <matt.harrison@traderinteractive.com>
2023-07-18 17:43:25 -0400

    TIDE-2 replaced ajax calls with dummy data references.

--- src/stores/SearchResultStore.ts
+++ src/stores/SearchResultStore.ts
@@ -1,43 +1,18 @@
-import axios from 'axios';
 import { defineStore } from 'pinia';
 
-import type { Raw } from '@/types/Raw';
 import type { Vehicle } from '@/types/Vehicle';
 
-import { mapResults } from '@/utilities/map';
+// Dummy API response to circumvent local/prod cross origin violation.
+import { dummyVehicles } from '@/data/dummy-vehicles';
 
 type State = {
   vehicles: Vehicle[];
 };
 
-const adData = (window as any).adData || null;
-
 export const useSearchResultStore = defineStore('searchResultStore', {
   actions: {
     getVehicles() {
-      const params = {
-        ad_id: adData.id || '',
-        dealer_group_id: '',
-        dealer_id: adData.dealerId || '',
-        make: adData.details.Make || '',
-        make_id: '',
-        model: adData.details.Model || '',
-      };
-
-      const query = new URLSearchParams(params);
-      const querystring = query.toString();
-
-      axios
-        .get(`/search-results-data/vdp-featured?${querystring}`)
-        .then((response) => {
-          const vehiclesRaw: Raw[] = response.data.results;
-          const vehicles: Vehicle[] = mapResults(vehiclesRaw);
-
-          this.setVehicles(vehicles);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
+      this.setVehicles(dummyVehicles);
     },
     setVehicles(vehicles: Vehicle[]) {
       this.vehicles = vehicles;
