a47272f5ae56c6b5b822e9f09484efc0c084fc7a
Matt Harrison <matt.harrison@traderinteractive.com>
2023-07-19 09:32:29 -0400

    TIDE-2 simplify project for demo purposes.

--- vite.config.ts
+++ vite.config.ts
@@ -15,9 +15,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         'app-single': resolve(`${__dirname}/src/contexts`, 'app-single.ts'),
-        'app-srp': resolve(`${__dirname}/src/contexts`, 'app-srp.ts'),
-        'app-style-guide': resolve(`${__dirname}/src/contexts`, 'app-style-guide.ts'),
-        'app-vdp': resolve(`${__dirname}/src/contexts`, 'app-vdp.ts'),
       },
       output: {
         assetFileNames: 'assets/[name].[ext]',
@@ -31,18 +28,6 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  server: {
-    host: true,
-    port: 5173,
-    proxy: {
-      '^/search-results-data/.*': {
-        changeOrigin: true,
-        secure: true,
-        target: 'https://www.rvtrader.com/',
-        ws: true,
-      },
     },
   },
   test: {
