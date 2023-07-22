7c57e9d364e5da08549721f8e334e2c94baf7610
Matt Harrison <matt.harrison@traderinteractive.com>
2023-07-22 16:36:19 -0400

    TIDE implemented realm-switching via querystring param.

--- src/config/main.config.ts
+++ src/config/main.config.ts
@@ -1,4 +1,7 @@
 import type { RealmConfig } from '../types/RealmConfig';
+import type { Realm } from '../types/Realm';
+
+import { REALM } from '../types/Realm';
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import * as aero from './aero.config';
@@ -12,4 +15,22 @@ import * as rv from './rv.config';
 import * as snow from './snow.config';
 import * as truck from './truck.config';
 
-export const realm: RealmConfig = rv.realm;
+// TIDE: Override Realm selection based on querystring param for DEMO purposes only.
+const params = new URLSearchParams(window.location.search);
+const paramRealm = params.get('realm');
+const realms = {
+  aero,
+  atv,
+  boatline,
+  cycle,
+  equip,
+  pwc,
+  rv,
+  snow,
+  truck,
+};
+
+const realmCode: Realm = paramRealm && Object.keys(realms).includes(paramRealm) ? (paramRealm as Realm) : REALM.RV;
+
+export const realm: RealmConfig = realms[realmCode]?.realm;
+// export const realm: RealmConfig = rv.realm;
