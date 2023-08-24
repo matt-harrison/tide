53303118bdc15971e5892cca982e3e86920f06d0
Matt Harrison <matt.harrison@traderinteractive.com>
2023-08-24 13:16:21 -0400

    TIDE-9 added BasicToggle stories.

---
+++ src/stories/BasicToggle.stories.ts
@@ -0,0 +1,57 @@
+import BasicToggle from '@/components/BasicToggle.vue';
+import { formatSnippet } from '@/utilities/storybook';
+
+const parameters = {
+  docs: {
+    source: {
+      format: false,
+      language: 'html',
+      transform: formatSnippet,
+    },
+  },
+};
+
+const formatArgs = (args: any) => {
+  return { args };
+};
+
+const render = (args: any) => ({
+  components: { BasicToggle },
+  setup() {
+    return formatArgs(args);
+  },
+  template: '<BasicToggle v-bind="args" />',
+  updated() {
+    return formatArgs(args);
+  },
+});
+
+export default {
+  argTypes: {
+    isActive: {
+      control: 'boolean',
+      table: {
+        defaultValue: { summary: 'False' },
+      },
+    },
+  },
+  component: BasicToggle,
+  tags: ['autodocs'],
+  title: 'Basic Components/BasicToggle',
+};
+
+export const BasicToggleInactive = {
+  args: {
+    isActive: false,
+  },
+  parameters,
+  render,
+};
+
+export const BasicToggleActive = {
+  args: {
+    isActive: true,
+  },
+  parameters,
+  render,
+};
