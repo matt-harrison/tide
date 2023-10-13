dc52db3ef28f58b242ef3f8e86ea6e35e3033239
Matt Harrison <matt.harrison@traderinteractive.com>
2023-10-13 13:07:27 -0400

    TIDE-34 implemented custom action to demo dynamic click listener for BasicChipInput.

--- src/stories/BasicChipInput.stories.ts
+++ src/stories/BasicChipInput.stories.ts
@@ -1,5 +1,19 @@
+import { action } from '@storybook/addon-actions';
+
+import type { StoryContext } from '@storybook/vue3';
+
 import BasicChipInput from '@/components/BasicChipInput.vue';
-import { click, formatSnippet } from '@/utilities/storybook';
+import { click } from '@/utilities/storybook';
+
+const formatSnippet = (code: string, context: StoryContext) => {
+  const { args } = context;
+
+  const argsWithValues: string[] = [];
+
+  if (args.label) argsWithValues.push(`label="${args.label}"`);
+
+  return `<BasicChipInput ${argsWithValues.join(' ')} @click="${args.click}" />`;
+};
 
 const parameters = {
   docs: {
@@ -10,6 +24,17 @@ const parameters = {
     },
   },
 };
+
+const render = (args: any) => ({
+  components: { BasicChipInput },
+  methods: {
+    handleClick: (event: Event) => {
+      if (args.click) action(args.click)(event);
+    },
+  },
+  setup: () => ({ args }),
+  template: `<BasicChipInput @click="handleClick" v-bind="args" />`,
+});
 
 export default {
   argTypes: {
@@ -24,11 +49,12 @@ export default {
     },
   },
   args: {
-    click: 'handleClick',
+    click: 'doSomething',
     label: 'Demo',
   },
   component: BasicChipInput,
   parameters,
+  render,
   tags: ['autodocs'],
   title: 'Basic Components/BasicChipInput',
 };
