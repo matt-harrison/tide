14d10d62a3f6ac1aa518e70eb1e787c405cb97b4
Matt Harrison <matt.harrison@traderinteractive.com>
2024-03-05 13:04:09 -0500

    TIDE-47 whitelisted working components for demo purposes.

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -11,7 +11,32 @@ const config: StorybookConfig = {
   },
   stories: [
     '../src/stories/Welcome.mdx',
-    '../src/stories/*.stories.ts',
+    // '../src/stories/*.stories.ts',
+
+    '../src/stories/Demo*.stories.ts',
+    '../src/stories/Foundations*.stories.ts',
+
+    '../src/stories/BasicAccordionItem.stories.ts',
+    '../src/stories/BasicAlert.stories.ts',
+    '../src/stories/BasicBreadCrumbs.stories.ts',
+    '../src/stories/BasicButton.stories.ts',
+    '../src/stories/BasicButtonIcon.stories.ts',
+
+    '../src/stories/BasicButtonPagination.stories.ts',
+    '../src/stories/BasicCard.stories.ts',
+    '../src/stories/BasicCarousel.stories.ts',
+    '../src/stories/BasicChipAction.stories.ts',
+    '../src/stories/BasicChipFilter.stories.ts',
+
+    '../src/stories/BasicChipInput.stories.ts',
+    '../src/stories/BasicColumns.stories.ts',
+    '../src/stories/BasicDivider.stories.ts',
+    '../src/stories/BasicIcon.stories.ts',
+    '../src/stories/BasicPagination.stories.ts',
+
+    '../src/stories/BasicSeoLinks.stories.ts',
+    '../src/stories/BasicTabs.stories.ts',
+    '../src/stories/BasicToggle.stories.ts',
   ],
 };
 export default config;
